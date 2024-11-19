7dacab96298f914d9e376a8a093e05a7087d383d
matheus.araujo <matheus.araujo@trinusco.com.br>
2024-11-18 21:59:50 -0300

    subscriptions

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,9 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import { useGetChat } from "../../hooks/useGetChat";
-import { Divider, IconButton, InputBase, Paper, Stack } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useCreateMessage } from "../../hooks/useCreateMessage";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useGetMessages } from "../../hooks/useGetMessages";
+import { useMessageCreated } from "../../hooks/useMessageCreated";
 
 const Chat = () => {
   const params = useParams();
@@ -11,10 +23,62 @@ const Chat = () => {
   const chatId = params._id!;
   const { data } = useGetChat({ _id: chatId });
   const [createMessage] = useCreateMessage();
+  const { data: messages } = useGetMessages({ chatId });
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const location = useLocation();
+
+  useMessageCreated({ chatId });
+
+  const scrollToBottom = () => divRef.current?.scrollIntoView();
+
+  useEffect(() => {
+    setMessage("");
+    scrollToBottom();
+  }, [location.pathname, messages]);
+
+  const handleCreateMessage = async () => {
+    await createMessage({
+      variables: { createMessageInput: { content: message, chatId } },
+    });
+    setMessage("");
+    scrollToBottom();
+  };
 
   return (
     <Stack sx={{ height: "100%", justifyContent: "space-between" }}>
       <h1>{data?.chat.name}</h1>
+      <Box sx={{ maxHeight: "70vh", overflow: "auto" }}>
+        {messages &&
+          [...messages.messages]
+            .sort(
+              (messageA, messageB) =>
+                new Date(messageA.createdAt).getTime() -
+                new Date(messageB.createdAt).getTime()
+            )
+            .map((message) => (
+              <Grid container alignItems="center" marginBottom="1rem">
+                <Grid item xs={2} lg={1}>
+                  <Avatar src="" sx={{ width: 52, height: 52 }} />
+                </Grid>
+                <Grid item xs={10} lg={11}>
+                  <Stack>
+                    <Paper sx={{ width: "fit-content" }}>
+                      <Typography sx={{ padding: "0.9rem" }}>
+                        {message.content}
+                      </Typography>
+                    </Paper>
+                    <Typography
+                      variant="caption"
+                      sx={{ marginLeft: "0.25rem" }}
+                    >
+                      {new Date(message.createdAt).toLocaleTimeString()}
+                    </Typography>
+                  </Stack>
+                </Grid>
+              </Grid>
+            ))}
+        <div ref={divRef}></div>
+      </Box>
       <Paper
         sx={{
           p: "2px 4px",
@@ -22,6 +86,7 @@ const Chat = () => {
           justifySelf: "flex-end",
           alignItems: "center",
           width: "100%",
+          margin: "1rem 0",
         }}
       >
         <InputBase
@@ -29,14 +94,15 @@ const Chat = () => {
           onChange={(event) => setMessage(event.target.value)}
           value={message}
           placeholder="Message"
+          onKeyDown={async (event) => {
+            if (event.key === "Enter") {
+              await handleCreateMessage();
+            }
+          }}
         />
         <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
         <IconButton
-          onClick={() => {
-            createMessage({
-              variables: { createMessageInput: { content: message, chatId } },
-            });
-          }}
+          onClick={handleCreateMessage}
           color="primary"
           sx={{ p: "10px" }}
         >
--- src/hooks/useCreateMessage.ts
+++ src/hooks/useCreateMessage.ts
@@ -1,18 +1,23 @@
 import { useMutation } from "@apollo/client";
 import { graphql } from "../gql";
+import { updateMessages } from "../cache/messages";
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
     createMessage(createMessageInput: $createMessageInput) {
-      _id
-      content
-      createdAt
+      ...MessageFragment
     }
   }
 `);
 
 const useCreateMessage = () => {
-  return useMutation(createMessageDocument);
+  return useMutation(createMessageDocument, {
+    update(cache, { data }) {
+      if (data?.createMessage) {
+        updateMessages(cache, data.createMessage);
+      }
+    },
+  });
 };
 
 export { useCreateMessage };
